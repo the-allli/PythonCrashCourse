d02b37fc480921f4a8324db54b54955c3c2d4866
Ali <ha5403905@gmail.com>
2025-07-16 15:13:31 +0500

    loops in python

---
+++ 4_Loops/main.py
@@ -0,0 +1,17 @@
+# While loop
+while condition:
+    # do something
+
+# For loop
+for item in collection:
+    # do something
+
+# Break, continue
+for x in range(5):
+    if x == 2:
+        continue
+    if x == 4:
+        break
+    print(x)
+else:
+    print("Done!")

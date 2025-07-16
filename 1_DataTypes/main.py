85193fa21b0d8474af54734440e51947fbbf31c4
Ali <ha5403905@gmail.com>
2025-07-16 14:53:14 +0500

    data types in python

---
+++ 1_DataTypes/main.py
@@ -0,0 +1,21 @@
+# Data Types
+# Int
+a = 20
+# Float
+b = 20.5
+# Complex
+c = 3+20j
+# Boolean
+d = True
+# String
+e = "Hello, World!"
+# List
+f = [1, 2, 3, 4, 5]
+# Tuple
+g = (1, 2, 3, 4, 5)
+# Set
+h = {1, 2, 3, 4, 5}
+# Dictionary
+i = {"name": "Alice", "age": 30, "city": "New York"}
+# NoneType
+j = None

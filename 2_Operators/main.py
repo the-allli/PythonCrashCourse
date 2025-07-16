3b84d3c6882cb4c0b8778d329aca719bb0b30bcb
Ali <ha5403905@gmail.com>
2025-07-16 15:07:17 +0500

    operators in python

---
+++ 2_Operators/main.py
@@ -0,0 +1,12 @@
+# Arithmatic Operators
+x+y, x-y, x*y, x/y, x%y, x**y, x//y
+# Comparison Operators
+x==y, x!=y, x>y, x<y, x>=y, x<=y
+# Assignment Operators
+x=5, x+=3, x-=3, x*=3, x/=3, x%=3, x//=3, x**=3,
+# Logical Operators
+and, or, not
+# Identity Operators
+is, is not
+# Membership Operators
+in, not in
